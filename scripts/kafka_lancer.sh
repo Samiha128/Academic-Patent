ccb21c8341287fe1a03622949bd86d209cad69d4
samiha el mansouri <120471620+Samiha128@users.noreply.github.com>
2025-01-23 19:38:56 +0100

    Update kafka_lancer.sh

--- scripts/kafka_lancer.sh
+++ scripts/kafka_lancer.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-pip install requests python-dotenv confluent-kafka
-# Lancer un topic (exemple : création d'un topic)
+
 docker-compose exec kafka kafka-topics --create --topic scopus --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server localhost:29092
-# Lancer un consumer pour consommer les messages du topic
-docker-compose exec kafka kafka-console-consumer --topic scopus --from-beginning --bootstrap-server localhost:29092
